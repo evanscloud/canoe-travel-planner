eb370a3edfda937f506705ebc0c188e753619686
evanscloud <dijkstrascloud@gmail.com>
2016-06-20 12:49:25 -0400

    Add relationships to models

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,4 @@
 class Destination < ActiveRecord::Base
+  belongs_to :user
+  has_many :lists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :destinations
+  has_many :lists, :through => :destinations
+  has_secure_password
 end
