56690004a333a7cbe3f417c6777e41fda97ae088
evanscloud <dijkstrascloud@gmail.com>
2016-06-21 14:41:45 -0400

    Update trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -22,14 +22,14 @@ class TripsController < ApplicationController
       erb :'/trips/new', locals: {message: "There seems to be an error. Please try again."}
     else
       user = current_user
-      @destination = Trip.create(name: params[:name], location: params[:location], user_id: user.id)
-      redirect "/trips/#{@destination.id}"
+      @trip = Trip.create(name: params[:name], location: params[:location], user_id: user.id)
+      redirect "/trips/#{@trip.id}"
     end
   end
 
   get '/trips/:id' do
     if is_logged_in?
-      @destination = Trip.find(params[:id])
+      @trip = Trip.find(params[:id])
       erb :'/trips/show'
     else
       erb :'/users/login', locals: {message: "Access denied. Please log-in to view."}
@@ -38,11 +38,11 @@ class TripsController < ApplicationController
 
   get '/trips/:id/edit' do
     if is_logged_in?
-      @destination = Trip.find(params[:id])
-      if @destination.user_id == session[:user_id]
+      @trip = Trip.find(params[:id])
+      if @trip.user_id == session[:user_id]
         erb :'/trips/edit'
       else
-        erb :'/trips', locals: {message: "Sorry, you do not have permission to edit this destination."}
+        erb :'/trips', locals: {message: "Sorry, you do not have permission to edit this trip."}
       end
     else
       erb :'/users/login', locals: {message: "Access denied. Please log-in to view."}
@@ -51,18 +51,18 @@ class TripsController < ApplicationController
 
   patch '/trips/:id' do
     if params[:name].empty? || params[:location].empty?
-      @destination = Trip.find(params[:id])
+      @trip = Trip.find(params[:id])
       erb :'/trips/edit', locals: {message: "There seems to be an error. Please try again."}
     else
-      @destination = Trip.find(params[:id])
-      @destination.update(name: params[:name], location: params[:location])
-      redirect "/trips/#{@destination.id}"
+      @trip = Trip.find(params[:id])
+      @trip.update(name: params[:name], location: params[:location])
+      redirect "/trips/#{@trip.id}"
     end
   end
 
   delete '/trips/:id/delete' do
-    @destination = Trip.find(params[:id])
-    @destination.delete
+    @trip = Trip.find(params[:id])
+    @trip.delete
     redirect '/trips'
   end
 
